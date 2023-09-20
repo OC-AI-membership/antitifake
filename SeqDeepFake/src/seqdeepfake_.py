ea21f6c2102a54572b8798f6cf283fbc42224ab0
riverallzero <kallzero1008@jbnu.ac.kr>
2023-09-20 01:37:59 +0000

    Integrating seqdeepfake facial_attributes and facial_components

--- SeqDeepFake/src/seqdeepfake_.py
+++ SeqDeepFake/src/seqdeepfake_.py
@@ -7,22 +7,16 @@ from SeqDeepFake.models.configuration import Config
 import warnings
 warnings.filterwarnings(action='ignore')
 
-attribute_checkpoint = './SeqDeepFake/results/facial_attributes/best_model_adaptive.pt'
-component_checkpoint = './SeqDeepFake/results/facial_components/best_model_adaptive.pt'
+model_checkpoint = './SeqDeepFake/results/best_model_adaptive.pt'
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model_config = Config('./SeqDeepFake/configs/r50.json')
 
-attribute_model = SeqFakeFormer.build_model(model_config)
-attribute_model.load_state_dict(torch.load(attribute_checkpoint, map_location=device)['best_state_dict_adaptive'])
-attribute_model.to(device)
-attribute_model.eval()
-
-component_model = SeqFakeFormer.build_model(model_config)
-component_model.load_state_dict(torch.load(component_checkpoint, map_location=device)['best_state_dict_adaptive'])
-component_model.to(device)
-component_model.eval()
+model = SeqFakeFormer.build_model(model_config)
+model.load_state_dict(torch.load(model_checkpoint, map_location=device)['best_state_dict_adaptive'])
+model.to(device)
+model.eval()
 
 
 def create_caption_and_mask(cfg):
@@ -35,34 +29,29 @@ def create_caption_and_mask(cfg):
     return caption_template, mask_template
 
 
-attributes_mapping = {
-    0: 'NA',
-    1: 'Bangs',
-    2: 'Eyeglasses',
-    3: 'Beard',
-    4: 'Smiling',
-    5: 'Young'
-}
-attributes_results = {
-    'Eyeglasses': 'Eyes',
-    'Smiling': 'Mouth',
-    'Young': 'Face'
+mapping = {
+    0: 'None',
+    1: 'bangs',
+    2: 'eyeglasses',
+    3: 'beard',
+    4: 'smiling',
+    5: 'young',
+    6: 'nose',
+    7: 'eye',
+    8: 'eyebrow',
+    9: 'lip',
+    10: 'hair'
 }
 
-components_mapping = {
-    0: 'NA',
-    1: 'nose',
-    2: 'eye',
-    3: 'eyebrow',
-    4: 'lib',
-    5: 'hair'
-}
-components_results = {
+mapping_results = {
+    'eyeglasses': 'eyes',
+    'smiling': 'mouth',
+    'young': 'face',
     'lib': 'mouth',
 }
 
 
-def attribute_predict(image_path):
+def model_predict(image_path):
     pil_image = Image.open(image_path)
     image_transform = transforms.Compose([
         transforms.Resize((256, 256)),
@@ -73,7 +62,7 @@ def attribute_predict(image_path):
     # Generate a caption using the model
     caption, cap_mask = create_caption_and_mask(model_config)
     for i in range(model_config.max_position_embeddings - 1):
-        predictions = attribute_model(image, caption, cap_mask)
+        predictions = model(image, caption, cap_mask)
         predictions = predictions[:, i, :]
         predicted_id = torch.argmax(predictions, axis=-1)
 
@@ -91,50 +80,9 @@ def attribute_predict(image_path):
 
     results = caption[0].tolist()
 
-    true_or_fake = 'False' if all(val == 0 for val in results) else 'True'
-    mapped = [attributes_mapping[val] for val in results if val != 0]
-    mapped_results = [attributes_results[result] if result in attributes_results else result for result in mapped]
-
-    # Mapping num - attribute
+    true_false = 'False' if all(val == 0 for val in results) else 'True'
+    mapped = [mapping[val] for val in results if val != 0]
+    mapped_results = [mapping_results[result] if result in mapping_results else result for result in mapped]
     result_string = '. '.join(mapped_results) + '.'
 
-    return true_or_fake, result_string, mapped
-
-
-def component_predict(image_path):
-    pil_image = Image.open(image_path)
-    image_transform = transforms.Compose([
-        transforms.Resize((256, 256)),
-        transforms.ToTensor(),
-    ])
-    image = image_transform(pil_image).unsqueeze(0).to(device)
-
-    # Generate a caption using the model
-    caption, cap_mask = create_caption_and_mask(model_config)
-    for i in range(model_config.max_position_embeddings - 1):
-        predictions = component_model(image, caption, cap_mask)
-        predictions = predictions[:, i, :]
-        predicted_id = torch.argmax(predictions, axis=-1)
-
-        if predicted_id[0] == model_config.EOS_token_id:
-            caption = caption[:, 1:]
-            zero = torch.zeros_like(caption)
-            caption = torch.where(caption == model_config.PAD_token_id, zero, caption)
-            break
-
-        caption[:, i + 1] = predicted_id[0]
-        cap_mask[:, i + 1] = False
-
-    if caption.shape[1] == 6:
-        caption = caption[:, 1:]
-
-    results = caption[0].tolist()
-
-    true_or_fake = 'False' if all(val == 0 for val in results) else 'True'
-    mapped = [components_mapping[val] for val in results if val != 0]
-    mapped_results = [components_results[result] if result in components_results else result for result in mapped]
-
-    # Mapping num - component
-    result_string = '. '.join(mapped_results) + '.'
-
-    return true_or_fake, result_string, mapped
+    return true_false, result_string, mapped
--- app.py
+++ app.py
@@ -1,22 +1,11 @@
-from SeqDeepFake.src.seqdeepfake_ import attribute_predict, component_predict
+from SeqDeepFake.src.seqdeepfake_ import model_predict
 from GroundingDINO.src.groundingdino_ import dino_predict
 from TruFor.src.trufor_ import trufor
 import gradio as gr
 
 
-def attribute_make_result(image_path):
-    # SeqDeepFake - attribute model
-    results = attribute_predict(image_path)
-    # GroundingDINO
-    image_output = dino_predict(image_path, results[1])
-
-    return results[0], results[2], image_output
-
-
-def component_make_result(image_path):
-    # SeqDeepFake - component model
-    results = component_predict(image_path)
-    # GroundingDINO
+def make_result(image_path):
+    results = model_predict(image_path)
     image_output = dino_predict(image_path, results[1])
 
     return results[0], results[2], image_output
@@ -26,29 +15,15 @@ with gr.Blocks() as demo:
     gr.Markdown("DeepFake Detection Demo - OC AI Membership")
 
     with gr.Tab("SeqDeepfake"):
-        with gr.Tab("Facial Attributes"):
-            with gr.Row():
-                with gr.Column(scale=1, min_width=600):
-                    image_path = gr.Image(type='filepath', value='./.test/attribute.png')
-                    btn = gr.Button(value='Predict')
-                    result1_output = gr.Textbox(label='DeepFake')
-                    result2_output = gr.Textbox(label='Attribute')
-
-                with gr.Column(scale=2, min_width=400):
-                    result3_output = gr.Image(type='filepath', width=400)
-                    btn.click(attribute_make_result, inputs=[image_path], outputs=[result1_output, result2_output, result3_output])
-
-        with gr.Tab("Facial Components"):
-            with gr.Row():
-                with gr.Column(scale=1, min_width=600):
-                    image_path = gr.Image(type='filepath', value='./.test/component.png')
-                    btn = gr.Button(value='Predict')
-                    result1_output = gr.Textbox(label='DeepFake')
-                    result2_output = gr.Textbox(label='Component')
-
-                with gr.Column(scale=2, min_width=400):
-                    result3_output = gr.Image(type='filepath', width=400)
-                    btn.click(component_make_result, inputs=[image_path], outputs=[result1_output, result2_output, result3_output])
+        with gr.Row():
+            with gr.Column(scale=1, min_width=600):
+                image_path = gr.Image(type='filepath', value='./.test/seqdeepfake.png')
+                btn = gr.Button(value='predict')
+                result1_output = gr.Textbox(label='is it deepfake?')
+                result2_output = gr.Textbox(label='manipulated')
+            with gr.Column(scale=2, min_width=400):
+                result3_output = gr.Image(type='filepath', width=400)
+        btn.click(make_result, inputs=[image_path], outputs=[result1_output, result2_output, result3_output])
 
     with gr.Tab("Tru For"):
         with gr.Row():
